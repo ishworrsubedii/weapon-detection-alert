8ed9278eb186ffdabe21f02a1eb01e7e149ceef6
Ishwor Subedi <ishworhero21@gmail.com>
2023-12-25 23:11:07 +0545

    added code for ipcam logger

--- services/__init__.py
+++ services/__init__.py
@@ -2,19 +2,42 @@ import os
 import sys
 import logging
 
-logging_str = "[%(asctime)s:%(levelname)s:%(module)s:%(message)s]"
 
-log_dir = "logs"
-log_path = os.path.join(log_dir, "gun_det.log")
-os.makedirs(log_dir, exist_ok=True)
+def main_sys_logger(log_dir="logs"):
+    logging_str = "[%(asctime)s:%(levelname)s:%(module)s:%(message)s]"
 
-logging.basicConfig(
-    level=logging.INFO,
-    format=logging_str,
+    log_path = os.path.join(log_dir, "gun_det.log")
+    os.makedirs(log_dir, exist_ok=True)
 
-    handlers=[
-        logging.FileHandler(log_path),
-        logging.StreamHandler(sys.stdout)
-    ]
-)
-logger = logging.getLogger("gun_detection_project")
+    logging.basicConfig(
+        level=logging.INFO,
+        format=logging_str,
+
+        handlers=[
+            logging.FileHandler(log_path),
+            logging.StreamHandler(sys.stdout)
+        ]
+    )
+    logger = logging.getLogger("main_sys_logger")  # Changed logger name
+
+    return logger
+
+
+def ipcam_logger(log_dir="logs"):
+    logging_str = "[%(asctime)s:%(levelname)s:%(module)s:%(message)s]"
+
+    log_path = os.path.join(log_dir, "ipcam.log")
+    os.makedirs(log_dir, exist_ok=True)
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format=logging_str,
+
+        handlers=[
+            logging.FileHandler(log_path),
+            logging.StreamHandler(sys.stdout)
+        ]
+    )
+    logger = logging.getLogger("ipcam_logger")  # Changed logger name
+
+    return logger
