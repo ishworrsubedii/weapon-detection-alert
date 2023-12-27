13162215920812c23dc266acd42c295c57f9ffff
Ishwor Subedi <ishworhero21@gmail.com>
2023-12-27 22:27:29 +0545

    added one more logger for detection service

--- services/__init__.py
+++ services/__init__.py
@@ -3,10 +3,18 @@ import sys
 import logging
 
 
-def main_sys_logger(log_dir="logs"):
+def configure_logger(logger_name, log_filename, log_dir="logs"):
+    """
+    Configures a logger with specified parameters.
+
+    :param logger_name: Name of the logger.
+    :param log_filename: Name of the log file.
+    :param log_dir: Directory where logs will be stored. Defaults to "logs".
+    :return: Configured logger.
+    """
     logging_str = "[%(asctime)s:%(levelname)s:%(module)s:%(message)s]"
 
-    log_path = os.path.join(log_dir, "gun_det.log")
+    log_path = os.path.join(log_dir, log_filename)
     os.makedirs(log_dir, exist_ok=True)
 
     logging.basicConfig(
@@ -18,26 +26,36 @@ def main_sys_logger(log_dir="logs"):
             logging.StreamHandler(sys.stdout)
         ]
     )
-    logger = logging.getLogger("main_sys_logger")  # Changed logger name
+    logger = logging.getLogger(logger_name)
 
     return logger
+
+
+def main_sys_logger(log_dir="logs"):
+    """
+    Configures and returns the main system logger.
+
+    :param log_dir: Directory where logs will be stored. Defaults to "logs".
+    :return: Main system logger.
+    """
+    return configure_logger("main_sys_logger", "gun_det.log", log_dir)
 
 
 def ipcam_logger(log_dir="logs"):
-    logging_str = "[%(asctime)s:%(levelname)s:%(module)s:%(message)s]"
+    """
+    Configures and returns the IP camera logger.
 
-    log_path = os.path.join(log_dir, "ipcam.log")
-    os.makedirs(log_dir, exist_ok=True)
+    :param log_dir: Directory where logs will be stored. Defaults to "logs".
+    :return: IP camera logger.
+    """
+    return configure_logger("ipcam_logger", "ipcam.log", log_dir)
 
-    logging.basicConfig(
-        level=logging.INFO,
-        format=logging_str,
 
-        handlers=[
-            logging.FileHandler(log_path),
-            logging.StreamHandler(sys.stdout)
-        ]
-    )
-    logger = logging.getLogger("ipcam_logger")  # Changed logger name
+def detection_logger(log_dir="logs"):
+    """
+    Configures and returns the detection logger.
 
-    return logger
+    :param log_dir: Directory where logs will be stored. Defaults to "logs".
+    :return: Detection logger.
+    """
+    return configure_logger("detection_logger", "detection.log", log_dir)
