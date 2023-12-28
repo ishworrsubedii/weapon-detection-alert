a1f8723a010743059a46c35183420d17b4f696f3
Ishwor Subedi <ishworhero21@gmail.com>
2023-12-28 17:46:26 +0545

    logger setup

--- services/__init__.py
+++ services/__init__.py
@@ -17,16 +17,19 @@ def configure_logger(logger_name, log_filename, log_dir="logs"):
     log_path = os.path.join(log_dir, log_filename)
     os.makedirs(log_dir, exist_ok=True)
 
-    logging.basicConfig(
-        level=logging.INFO,
-        format=logging_str,
-
-        handlers=[
-            logging.FileHandler(log_path),
-            logging.StreamHandler(sys.stdout)
-        ]
-    )
     logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(logging_str)
+
+    file_handler = logging.FileHandler(log_path)
+    file_handler.setFormatter(formatter)
+
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
 
     return logger
 
