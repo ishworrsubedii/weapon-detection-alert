2880f7caa25b8531f379b2b4ad5782b91bc6199a
Ishwor Subedi <ishworhero21@gmail.com>
2023-12-28 18:20:14 +0545

    updated

--- services/image_capture_ipcam/capture_main.py
+++ services/image_capture_ipcam/capture_main.py
@@ -1,18 +1,27 @@
+"""
+Author: ishwor subedi
+Date: 2023-12-28
+Project Name:gun-detection
+
+"""
 import os
+from PIL import Image
 from datetime import datetime
 import threading
-import cv2 as cv
-from PIL import Image
 import imagehash
 import time
-from services import main_sys_logger, ipcam_logger
+import cv2 as cv
+from services import main_sys_logger
+from services import ipcam_logger
 from utils.settings import get_frame_save_dir
 
 
 class FrameSaver:
     def __init__(self, source, image_path_to_save, image_hash_threshold):
         self.source = source
-        self.image_path_to_save = image_path_to_save or get_frame_save_dir()
+        self.image_path_to_save = image_path_to_save
+        if self.image_path_to_save is None or not os.path.exists(self.image_path_to_save):
+            self.image_path_to_save = get_frame_save_dir()
         self.thread_running = False
         self.frame_save_thread = None
         self.cap = cv.VideoCapture(self.source)
@@ -26,13 +35,10 @@ class FrameSaver:
         self.thread_running = True
 
     def stop_stream(self):
-        try:
-            if self.thread_running:
-                self.thread_running = False
-                self.frame_save_thread.join()
-            self.cap.release()
-        except Exception as e:
-            self.ipcam_logger.exception(f"Error stopping stream: {e}")
+        if self.thread_running:
+            self.thread_running = False
+            self.frame_save_thread.join()
+        self.cap.release()
 
     def hashing_diff(self, prev_frame, current_frame):
         if prev_frame is None:
@@ -42,16 +48,8 @@ class FrameSaver:
             hash2 = imagehash.average_hash(Image.fromarray(current_frame))
             return hash2 - hash1
 
-    def save_frame(self, frame):
-        current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        name = os.path.join(self.image_path_to_save, f"{current_time}.jpg")
-        success = cv.imwrite(name, frame)
-        if success:
-            self.ipcam_logger.info(f"Saved {name}...")
-        else:
-            self.ipcam_logger.error(f"Failed to save image: {name}")
-
     def video_webcam_frame_extraction(self):
+
         try:
             previous_frame = None
             while self.cap.isOpened() and self.thread_running:
@@ -59,23 +57,18 @@ class FrameSaver:
                 if success:
                     hash_diff = self.hashing_diff(previous_frame, frame)
                     if hash_diff is None or hash_diff > self.image_hash_threshold:
-                        self.save_frame(frame)
+                        current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                        name = os.path.join(self.image_path_to_save, f"{current_time}.jpg")
+                        success = cv.imwrite(name, frame)  # Check for success
+                        if success:
+                            self.ipcam_logger.info(f"Saved {name}...")
+                        else:
+                            self.ipcam_logger.error(f"Failed to save image: {name}")
+                            # Consider retrying image saving or taking corrective actions here
                     previous_frame = frame
         except Exception as e:
             self.ipcam_logger.exception(f"Exception occurred: {e}")
-            self.stop_stream()
-
-
-if __name__ == '__main__':
-    source = "rtsp://192.168.1.106:8080/h264_opus.sdp"
-    image_path_to_save = "images/cam_images"
-    start_stop = FrameSaver(source, image_path_to_save, image_hash_threshold=5)
-
-    try:
-        start_stop.start_stream()
-        time.sleep(1)
-    except Exception as e:
-        start_stop.ipcam_logger.exception(f"Error starting stream: {e}")
-
-    finally:
-        start_stop.stop_stream()
+            try:
+                self.stop_stream()  # Attempt to stop stream gracefully
+            except Exception as e:
+                self.ipcam_logger.exception(f"Failed to stop stream: {e}")
