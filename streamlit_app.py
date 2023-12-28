560fdfea4ae9533ea24e6ac379e39ffd9de3eed8
Ishwor Subedi <ishworhero21@gmail.com>
2023-12-28 22:49:31 +0545

    added the api address of each function

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,53 +1,82 @@
-"""
-Author: ishwor subedi
-Date: 2023-12-28
-Project Name: gun-detection
-"""
 import os
+import requests
 import streamlit as st
-LOG_DIR = "logs"
+
+LOG_DIR = "logs"  # Adjust the log directory as needed
+
+# Update the API URL based on your FastAPI deployment
+API_URL = "http://localhost:8000"
 
 
-def main():
+def call_api(endpoint):
+    try:
+        response = requests.post(f"{API_URL}/{endpoint}")
+        return response.json()
+    except Exception as e:
+        return {"error": str(e)}
+
+
+def view_location():
+    st.write("Enter location information:")
+    location_info = st.text_input("Location:")
+
+    if st.button("View Location"):
+        st.write(f"Viewing Location: {location_info}")
+
+
+def view_logs():
+    log_files = [file for file in os.listdir(LOG_DIR) if file.endswith(".log")]
+
+    selected_log_file = st.selectbox("Select Log File", log_files)
+
+    file_path = os.path.join(LOG_DIR, selected_log_file)
+
+    try:
+        with open(file_path, 'r') as file:
+            file_contents = file.read()
+            st.write(f"### File Contents: {selected_log_file}")
+            st.code(file_contents)
+    except FileNotFoundError:
+        st.error("File not found. Please select a valid log file.")
+
+
+def main_streamlit():
     st.sidebar.header("Weapon Detection and Location Sharing Alert Systems")
 
     selected_option = st.sidebar.selectbox('Select an option',
-                                           ["Start IP Cam Server", "Start Detection service", "View Location", "Data", "Logs"])
+                                           ["Start IP Cam Server", "Stop IP Cam Server",
+                                            "Start Detection service", "Stop Detection service",
+                                            "View Location", "Logs"])
 
     if selected_option == "Start IP Cam Server":
         if st.button('Start Server'):
-            st.write("Server started!")
+            result = call_api("start_ipcam_server")
+            st.write(result)
+
+    elif selected_option == "Stop IP Cam Server":
+        if st.button('Stop Server'):
+            result = call_api("stop_ipcam_server")
+            st.write(result)
 
     elif selected_option == "Start Detection service":
-        if st.button("Start Detection server"):
-            st.write("Detection server started!")
+        if st.button('Start Server'):
+            result = call_api("start_detection_service")
+            st.write(result)
+
+    elif selected_option == "Stop Detection service":
+        if st.button('Stop Server'):
+            result = call_api("stop_detection_service")
+            st.write(result)
 
     elif selected_option == "View Location":
-        st.write("Enter location information:")
-        location_info = st.text_input("Location:")
-
-        if st.button("View Location"):
-            st.write(f"Viewing Location: {location_info}")
-
-    elif selected_option == "Data":
-        st.write("You selected 'Data'. Add code to handle this option.")
+        view_location()
 
     elif selected_option == "Logs":
-        log_files = [file for file in os.listdir(LOG_DIR) if file.endswith(".log")]
+        view_logs()
 
-        selected_log_file = st.selectbox("Select Log File", log_files)
-
-        file_path = os.path.join(LOG_DIR, selected_log_file)
-
-        try:
-            with open(file_path, 'r') as file:
-                file_contents = file.read()
-                st.write(f"### File Contents: {selected_log_file}")
-                st.code(file_contents)
-        except FileNotFoundError:
-            st.error("File not found. Please select a valid log file.")
-        st.write("You selected 'Logs'. Add code to handle this option.")
+    else:
+        st.write("You selected an option. Add code to handle this option.")
 
 
 if __name__ == '__main__':
-    main()
+    main_streamlit()
