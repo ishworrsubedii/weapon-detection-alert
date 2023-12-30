d0b777963b9007febc171c8b4079e8c8d57aea64
Ishwor Subedi <ishworhero21@gmail.com>
2023-12-30 21:42:05 +0545

    added location share feature

---
+++ services/location_share/location_share_service.py
@@ -0,0 +1,15 @@
+"""
+Created By: ishwor subedi
+Date: 2023-12-30
+"""
+
+
+class LocationShareService:
+    def __init__(self):
+        pass
+
+    def request_access_of_location_from_browser(self):
+        pass
+
+    def location_show(self):
+        pass
