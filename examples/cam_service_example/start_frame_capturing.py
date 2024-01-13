d8182c32884b3eaed681c8555e57360aecc7cdd9
Ishwor Subedi <ishworhero21@gmail.com>
2024-01-13 22:38:44 +0545

    Implemented Image Capture Service for IPCamera system

--- examples/cam_service_example/start_frame_capturing.py
+++ examples/cam_service_example/start_frame_capturing.py
@@ -3,42 +3,37 @@ import time
 from services.image_capture_service.capture_main import FrameSaver
 from services import ipcam_logger, main_sys_logger
 
+IPCAM_LOGGER = ipcam_logger()
+MAIN_SYS_LOGGER = main_sys_logger()
 
-class StartImageCaptureServiceExample:
+
+class ImageCaptureService:
     def __init__(self, flag_path, source, image_path_to_save, image_hash_threshold):
         self.flag_path = flag_path
         self.source = source
         self.image_path_to_save = image_path_to_save
         self.image_hash_threshold = image_hash_threshold
-        self.ipcam_logger = ipcam_logger()
-        self.main_sys_logger = main_sys_logger()
+        self.running = False
 
     def create_stop_flag(self):
         try:
-            if not os.path.exists(self.flag_path):
-                with open(self.flag_path, 'w') as flag_file:
-                    flag_file.write('False')  # Set initial content as 'False'
-                self.ipcam_logger.info("Flag file created and set to 'False'.")
-            else:
-                with open(self.flag_path, 'r+') as flag_file:
-                    flag_file.truncate(0)  # Clear the file content
-                    flag_file.write('False')  # Write 'False' to the cleared file
-                self.ipcam_logger.info("Flag file exists and set to 'False'.")
+            with open(self.flag_path, 'w') as flag_file:
+                flag_file.write('False')  # Set initial content as 'False'
+            IPCAM_LOGGER.info("Flag file created and set to 'False'.")
         except Exception as e:
-            self.ipcam_logger.error(f"Error creating flag file: {e}")
+            IPCAM_LOGGER.error(f"Error creating flag file: {e}")
 
     def check_stop_flag(self):
         try:
             if os.path.exists(self.flag_path):
                 with open(self.flag_path, 'r') as flag_file:
                     content = flag_file.read().strip()
-                    if content.lower() == "true":
-                        return True
+                    return content.lower() == "true"
             else:
                 self.create_stop_flag()  # Create the stop flag file if it doesn't exist
                 return False
         except Exception as e:
-            self.ipcam_logger.error(f"Error checking stop flag: {e}")
+            IPCAM_LOGGER.error(f"Error checking stop flag: {e}")
             return False
 
     def update_stop_flag(self, value):
@@ -46,31 +41,42 @@ class StartImageCaptureServiceExample:
             with open(self.flag_path, 'w') as flag_file:
                 flag_file.write(str(value))  # Write the provided value to the flag file
         except Exception as e:
-            self.ipcam_logger.error(f"Error updating stop flag: {e}")
+            IPCAM_LOGGER.error(f"Error updating stop flag: {e}")
 
     def start_service(self):
         try:
-            self.ipcam_logger.info(
-                "*************************Image Capturing service has been started.*************************\n")
-
+            IPCAM_LOGGER.info("Image Capturing service starting.")
+            self.running = True
             start_capture = FrameSaver(self.source, self.image_path_to_save, self.image_hash_threshold)
             start_capture.start_stream()
 
-            while True:
-                time.sleep(10)
+            while self.running:
+                time.sleep(1)
                 stop_flag = self.check_stop_flag()
+                IPCAM_LOGGER.debug(f"Stop flag status: {stop_flag}")
+
                 if stop_flag:
-                    self.ipcam_logger.info(
-                        "*************************Stop flag detected. Stopping all frame capturing services.*************************\n")
-                    start_capture.stop_stream()
-                    break  # Break out of the loop to stop capturing services
+                    IPCAM_LOGGER.info("Stop flag detected. Stopping all frame capturing services.")
+                    self.running = False
+                    stop_successful = start_capture.stop_stream()
+                    if stop_successful:
+                        IPCAM_LOGGER.info("Frame capturing services successfully stopped.")
+                    else:
+                        IPCAM_LOGGER.warning("Issue encountered while stopping frame capturing services.")
 
         except Exception as e:
-            self.main_sys_logger.exception(f"Error in setting up FrameSaver: {e}\n")
-            self.ipcam_logger.exception(f"Error in setting up FrameSaver: {e}\n")
+            MAIN_SYS_LOGGER.exception(f"Error in setting up FrameSaver: {e}")
+            IPCAM_LOGGER.exception(f"Error in setting up FrameSaver: {e}")
         finally:
             if 'start_capture' in locals():
-                start_capture.stop_stream()
+                stop_successful = start_capture.stop_stream()
+                if stop_successful:
+                    IPCAM_LOGGER.info("Frame capturing services successfully stopped in finally block.")
+                else:
+                    IPCAM_LOGGER.warning("Issue encountered while stopping frame capturing services in finally block.")
+
+    def stop_service(self):
+        self.update_stop_flag("True")
 
 
 if __name__ == "__main__":
@@ -79,5 +85,5 @@ if __name__ == "__main__":
     IMAGE_PATH_TO_SAVE = "images/cam_images"
     IMAGE_HASH_THRESHOLD = 5
 
-    image_capture_service = StartImageCaptureServiceExample(FLAG_PATH, SOURCE, IMAGE_PATH_TO_SAVE, IMAGE_HASH_THRESHOLD)
+    image_capture_service = ImageCaptureService(FLAG_PATH, SOURCE, IMAGE_PATH_TO_SAVE, IMAGE_HASH_THRESHOLD)
     image_capture_service.start_service()
